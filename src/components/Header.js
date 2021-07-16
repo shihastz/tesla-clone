1fe712065b177c7ce5a6d1f49d7be0ecd0a67022
shihastz <shihast0503@gmail.com>
2021-07-16 18:42:14 +0530

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import styled from "styled-components";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
+
+function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  //   const handleClick = (event) => {
+  //     // Prevent default events
+  //     event.preventDefault();
+
+  //     // Animate the body (html page) to scroll to the referring element
+  //     $("html, body").animate(
+  //       {
+  //         scrollTop: $($.attr(this, "href")).offset().top,
+  //       },
+  //       1000
+  //     );
+  //   };
+  console.log("cars", cars);
+  return (
+    <Container>
+      <a>
+        <img src="./images/logo.svg" alt="logo" />
+      </a>
+      <Menu>
+        {/* {cars &&
+          cars.map((car, index) => (
+            <a href="#" key={index}>
+              {car}
+            </a>
+          ))} */}
+        <a href="#model_s"> Model S</a>
+        <a href="model_3">Model 3</a>
+        <a href="#model_x"> Model X</a>
+        <a href="#model_y">Model Y</a>
+        <a href="#roof"> SOLAR ROOF</a>
+        <a href="#panel"> SOLAR PANELS</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-In</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+        <li>
+          <a href="#">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    flex-wrap: nowrap;
+    margin-right: 10px;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16px;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const CustomClose = styled(CloseIcon)``;
